418cef2ee0d8309f8285b4f9b3ef698ab9eb9a89
Gibies George <gibies.george@gov.in>
2023-09-26 11:53:25 +0530

    Minor change

--- site/mihir/set_env.sh
+++ site/mihir/set_env.sh
@@ -3,3 +3,6 @@ module load openmpi-4.0.3
 module load gnu/wgrib2/3.0.0
 module load cray-netcdf-hdf5parallel/4.6.1.3
 module load gnu/nco/4.7.1
+
+#/home/apps/SiteSoftwares/WRF_GNU_DMSM/WPS-4.3
+#/home/apps/SiteSoftwares/WRF_GNU_DMSM/WPS-4.3
