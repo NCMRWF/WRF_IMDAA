30ef009f9edec3d34507410fdf033c2802d8127f
Syam Sankar <syamsankar1@gmail.com>
2023-09-29 11:25:55 +0530

    Minor change

--- site/mihir/set_env.sh
+++ site/mihir/set_env.sh
@@ -1,5 +1,6 @@
 module load gcc/7.2.0
-module load openmpi-4.0.3
+#module load openmpi-4.0.3
+module load gnu/compiler/openmpi/1.10.6
 module load gnu/wgrib2/3.0.0
 module load cray-netcdf-hdf5parallel/4.6.1.3
 module load gnu/nco/4.7.1
